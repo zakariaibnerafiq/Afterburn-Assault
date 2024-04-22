48cd4d83344d6e82b59c926f27efe1f7a155535b
NotSoLucky <88020849+not-solucky@users.noreply.github.com>
2024-04-23 00:55:55 +0600

    423 project

---
+++ button.py
@@ -0,0 +1,86 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+from midpointLine import drawLine
+from midpointCircle import drawCircle
+from character import CHARACTERS
+
+class Button:
+    def __init__(self, pos, color, px, type, arr):
+        # type 1 = lines, type 2 = matrix , type 3 = txt
+        self.x = pos[0]
+        self.y = pos[1]
+        self.size = [0,0]
+        self.px = px
+        self.color = color 
+        self.type = type
+        self.arr = arr
+        
+        if type == 3:
+            self.size = [len(arr[0])*7*px, 8*px]
+        
+        if type == 1:
+            self.size = [max(arr[0]), max(arr[1])]
+    
+    def draw(self, boundary = False):
+        
+        if self.type == 1:
+            self.drawLines()
+            pass
+        elif self.type == 2:
+            # matrix logic
+            pass
+        elif self.type == 3:
+            self.textDraw()
+            
+        
+        if boundary:
+            self.drawBoundaryBox()
+            
+        
+    def drawBoundaryBox(self):
+        x1 = self.x
+        y1 = self.y
+        x2 = x1 + self.size[0]
+        y2 = y1 + self.size[1]
+        drawLine(x1,y1, x2,y1, [1,1,1], 1)
+        drawLine(x1,y1, x1,y2, [1,1,1], 1)
+        drawLine(x2,y1, x2,y2, [1,1,1], 1)
+        drawLine(x1,y2, x2,y2, [1,1,1], 1)
+    def drawLines(self):
+        x = self.x
+        y = self.y
+        for i in range(0,len(self.arr[0]), 2):
+            x_, y_ = self.arr[0][i], self.arr[1][i]
+            x__, y__ = self.arr[0][i+1], self.arr[1][i+1]
+            drawLine(x_+x,y_+y, x__+x,y__+y, self.color, self.px)
+    def textDraw(self):
+        # 01234
+        glColor3f(self.color[0], self.color[1], self.color[2])
+        glPointSize(self.px) 
+        glBegin(GL_POINTS)
+        string = self.arr[0]
+        x = self.x 
+        y = self.y
+        
+        for i in string:
+            matrix  = CHARACTERS[i]
+            
+            for j in range(7):
+                y_ = (7-j)*self.px + y
+                for k in range(6):
+                    if matrix[j][k] == 1:
+                        x_ = (k+1)*self.px + x
+                        glVertex2f(x_,y_)
+            x += 7*self.px
+        glEnd()
+        
+            
+        
+        
+    
+    def pressed(self, x, y):
+        if self.x < x < self.x + self.size[0] and self.y < y < self.y + self.size[1]:
+            return True
+        return False
+
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from midpointLine import drawLine
 from jet.jet import JET, JET_COLOR
 from jetClass import Jet
 
+from button import Button
 from hpbar import HealthBar
 # ===============Keyboard Listener================
 
@@ -37,6 +38,19 @@ def showScreen():
     glLoadIdentity()
     iterate()  
     # draw  
+    
+    if homepage:
+        number_button.draw(True)
+        lineButton.draw(True)
+    elif levelpage:
+        pass
+    elif gamepage:
+        pass
+    elif pausepage:
+        pass
+    elif gameoverpage:
+        pass
+    
     player.draw()
     player_healthbar.draw(player.health)
     num.draw(str(animation_loop), [100, 700],[1,1,1],5)
@@ -63,7 +77,16 @@ glutInitWindowSize(800, 800)
 glutInitWindowPosition(0, 0)
 wind = glutCreateWindow(b"Shoot the circles!")
 animation_loop = 0
+# Page Logic
+homepage = True
+levelpage = False
+gamepage = False
+pausepage = False
+gameoverpage = False
 
+number_button = Button([200,700], [1,1,1], 8, 3, ['EXIT'])
+
+lineButton = Button([200,600], [1,1,1], 6, 1, [[0, 0, 0, 50,0,50],[0, 50, 50, 25,0,25]])
 player = Jet([100,100], JET, JET_COLOR, 2)
 num = Number()
 player_healthbar = HealthBar(100,[50,10], [5,760])
